444497c60444e0f5deb3711d8be406f7cbde86db
earGO <rh.edwin@gmail.com>
2018-12-20 00:11:40 -0600

    added single post show

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ var express = require('express'),
     app = express(),
     port = 3000;
 
+//set basic env
 app.set('view engine','ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}));
 
+//set up MongoDB
 mongoose.connect('mongodb://localhost/restful_blog_app', { useNewUrlParser: true } );
 
 var blogSchema = new mongoose.Schema({
@@ -19,10 +21,12 @@ var blogSchema = new mongoose.Schema({
 
 var Blog = mongoose.model('Blog',blogSchema)
 
+// basic '/' route redirects to main index route
 app.get('/',(req,res) => {
     res.redirect('/blogs')
 })
 
+//main INDEX route
 app.get('/blogs',(req,res) => {
     Blog.find({},(err,posts) => {
         if(err){
@@ -33,13 +37,35 @@ app.get('/blogs',(req,res) => {
     })
 })
 
-app.get('/', (req, res) => {
-    res.send('got server up and running')
-})
-
+//NEW route
 app.get('/blogs/new',(req,res) => {
     res.render('new');
 })
+
+//CREATE route
+app.post('/blogs',(req,res) => {
+    let newPost = req.body.blog
+    Blog.create(newPost,(err,post) => {
+        if (err) {
+            console.log('uups got an error',err);
+        } else {
+            res.redirect('/blogs')
+        }
+    })
+})
+
+//SHOW route
+app.get('/blogs/:id',(req,res) => {
+    Blog.findById(req.params.id,(err,foundPost) =>{
+        if(err) {
+            console.log('error finding post',err)
+        } else {
+            res.render('show',{post:foundPost})
+        }
+    })
+})
+//UPDATE route
+//DESTROY route
 
 app.listen(port, function () {
     console.log('server up and running on port', port)
