fd21f369a64509ce04c792ab6d6fdd3682b1b956
earGO <rh.edwin@gmail.com>
2018-12-26 06:56:21 -0600

    finished as exersise

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-var express = require('express'),
-    bodyParser = require('body-parser'),
-    mongoose = require("mongoose"),
-    app = express(),
-    port = 3000;
+var express, bodyParser, mongoose, methodOverride, app, port, expressSanitizer;
+
+express = require('express');
+bodyParser = require('body-parser');
+mongoose = require("mongoose");
+methodOverride = require("method-override");
+expressSanitizer = require("express-sanitizer");
+app = express();
+port = 3000;
 
 //set basic env
 app.set('view engine','ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+app.use(expressSanitizer());
 
 //set up MongoDB
 mongoose.connect('mongodb://localhost/restful_blog_app', { useNewUrlParser: true } );
@@ -44,6 +50,7 @@ app.get('/blogs/new',(req,res) => {
 
 //CREATE route
 app.post('/blogs',(req,res) => {
+    req.body.blog.body = req.sanitize(req.body.blog.body)
     let newPost = req.body.blog
     Blog.create(newPost,(err,post) => {
         if (err) {
@@ -64,8 +71,51 @@ app.get('/blogs/:id',(req,res) => {
         }
     })
 })
+
+//EDIT page route
+app.get('/blogs/:id/edit',(req,res) => {
+    Blog.findById(req.params.id,(err,foundPost) =>{
+        if(err) {
+            console.log('error finding post',err)
+            res.redirect('/blogs')
+        } else {
+            res.render('edit',{post:foundPost})
+        }
+    })
+})
+
 //UPDATE route
+app.put('/blogs/:id',(req,res) => {
+    req.body.blog.body = req.sanitize(req.body.blog.body)
+    Blog.findOneAndUpdate(
+        req.params.id,
+        req.body.blog,
+        (err,updatedPost) => {
+            if (err) {
+                console.log('error updating post',err)
+                res.redirect('/blogs')
+            } else {
+                console.log('updated post, YAY!!!!')
+                res.redirect('/blogs/'+req.params.id)
+            }
+        }
+    )
+})
+
 //DESTROY route
+app.delete('/blogs/:id',(req,res) => {
+        //first destroy post
+    Blog.findOneAndRemove(req.params.id,(err) => {
+        if (err) {
+            console.log('error deleting post \n',err)
+            res.redirect('/blogs')
+        } else {
+            console.log('successfully deleted post')
+            res.redirect('/blogs')
+        }
+    })
+    //then redirrect somewhere
+ })
 
 app.listen(port, function () {
     console.log('server up and running on port', port)
